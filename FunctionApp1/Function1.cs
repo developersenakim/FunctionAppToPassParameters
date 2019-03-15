51b8540300f05fe412edef58663bd7b83bb638e4
Sena Kim <developersenakim@outlook.com>
2019-03-15 14:23:19 +0900

    outputting json type properly

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -11,11 +11,13 @@ using MySql.Data.MySqlClient;
 using System.Data.Common;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 namespace FunctionApp1
-{   
+{
     
     public static class Function1
-    {        
+    {
         [FunctionName("Function1")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
@@ -24,7 +26,6 @@ namespace FunctionApp1
             log.Info("C# HTTP trigger function processed a request.");
 
             MySqlConnection mysqlConnection = new MySqlConnection(GetEnvironmentVariable("mysql_connection"));
-            StudentData studentdata = new StudentData();
             List<StudentData> studentdataList = new List<StudentData>();
 
             // parse query parameter            
@@ -47,25 +48,25 @@ namespace FunctionApp1
                 string connectionState = mysqlConnection.State.ToString();
                 log.Info("connection {" + connectionState + "}.");
                 //excute query to call data
-                QueryEmployee(mysqlConnection,log, studentdata, studentdataList, departmentname);
+                QueryEmployee(mysqlConnection,log, studentdataList, departmentname);
                 mysqlConnection.Close();
 
             }
             catch (Exception ex)
             {
                 log.Warning("connecting to mysql failed : " + ex);
-            }          
+            }
 
             return departmentname == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a departmentname on the query string or in the request body")
-             : req.CreateResponse(HttpStatusCode.OK, studentdataList);
+             : req.CreateResponse(HttpStatusCode.OK, studentdataList, "application/json");
 
         }
         public static string GetEnvironmentVariable(string variablename)
         {
                return System.Environment.GetEnvironmentVariable(variablename);
         }
-        private static void QueryEmployee(MySqlConnection conn, TraceWriter log, StudentData studentdata, List<StudentData> studentdataList, string departmentname)
+        private static void QueryEmployee(MySqlConnection conn, TraceWriter log, List<StudentData> studentdataList, string departmentname)
         {
             string sql = "SELECT DEPARTMENT, NAME, EMAIL, STUDENT_GROUP, MANAGER_YN, ENABLED_USER_YN FROM bpdb.ba_student where DEPARTMENT like '"+ departmentname +"';";
             // create command
@@ -88,17 +89,17 @@ namespace FunctionApp1
             {             
                 
                 if (reader.HasRows)
-                {                    
+                {
                     while (reader.Read())
-                    {                        
-                        studentdata.Department = reader.GetString(0);
-                        studentdata.Name = reader.GetString(1);
-                        studentdata.Email = reader.GetString(2);
-                        studentdata.Student_group = reader.GetString(3);
-                        studentdata.Manager_yn = reader.GetString(4);
-                        studentdata.Enabled_user_yn = reader.GetString(5);
-                        studentdataList.Add(studentdata);
-                       // log.Info(readerstring + readerstring1 + readerstring2 +readerstring3);                        
+                    {
+                        studentdataList.Add(new StudentData(
+                            reader.GetString(0),
+                            reader.GetString(1),
+                           reader.GetString(2),
+                           reader.GetString(3),
+                           reader.GetString(4),
+                           reader.GetString(5)
+                           ));
                     }
                     reader.Close();
                 }
--- FunctionApp1/StudentClass.cs
+++ FunctionApp1/StudentClass.cs
@@ -6,16 +6,23 @@ using System.Threading.Tasks;
 
 namespace FunctionApp1
 {
-   
-        public class StudentData
-        {
-            public string Department { get; set; }
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public string Student_group { get; set; }
-            public string Manager_yn { get; set; }
-            public string Enabled_user_yn { get; set; }
-        }
-}
+    class StudentData
+    {
+        public string Department;
+        public string Name;
+        public string Email;
+        public string Student_group;
+        public string Manager_yn;
+        public string Enabled_user_yn;
 
-     
+        public StudentData(string department, string name, string email, string studentgroup, string manageryn, string enableuseryn)
+        {
+              Department = department;
+            Name = name;
+            Email = email;
+            Student_group = studentgroup;
+            Manager_yn = manageryn;
+            Enabled_user_yn = enableuseryn;
+        }
+    }
+}
